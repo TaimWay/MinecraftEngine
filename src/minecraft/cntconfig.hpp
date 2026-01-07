4063fd09e7e643f7e2103d71d941b6e9869afeed
TaimWay <taimway@gmail.com>
2026-01-07 16:51:57 +0800

    cntconfig.hpp: Declares some basic objects needed by the library

---
+++ src/minecraft/cntconfig.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef __MINECRAFT_ENGINE__CNTCONFIG_HPP__
+#define __MINECRAFT_ENGINE__CNTCONFIG_HPP__
+
+#include <filesystem>
+#include <string>
+
+namespace fs = std::filesystem;
+
+typedef int int32_t;
+using int64_t = int64_t;
+typedef long long int128_t;
+
+typedef unsigned int uint32_t;
+using uint64_t = uint64_t;  
+typedef unsigned long long uint128_t;
+
+using String = std::string;
+
+#endif // !__MINECRAFT_ENGINE__CNTCONFIG_HPP__
+    
