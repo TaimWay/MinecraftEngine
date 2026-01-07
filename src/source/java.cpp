3545878ca74e5020739428a4d31573ae25b788dd
TaimWay <taimway@gmail.com>
2026-01-07 17:05:47 +0800

    java.c++: Added copyright information

--- src/source/java.cpp
+++ src/source/java.cpp
@@ -1,3 +1,25 @@
+/*
+ * Minecraft Engine
+ * Copyright (C) 2026 TaimWay <taimway@gmail.com>
+ * 
+ * @File: source/java.cpp
+ * @Description: The implementation of minecraft/java.hpp
+ * @Ownership: TaimWay <taimway@gmail.com> - 01/07/2026
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <minecraft/java.hpp>
 
 namespace cnt {
